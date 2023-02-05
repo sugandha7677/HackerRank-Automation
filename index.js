492193c42596545fcdf2e1e915da71887c51c11e
Sugandha <sugandhatiwary7677@gmail.com>
2023-02-05 08:32:22 +0530

    done

--- index.js
+++ index.js
@@ -4,123 +4,114 @@ const codeObj = require('./codes')
 
 const loginLink = 'https://www.hackerrank.com/auth/login';
 const email = 'sugandhatiwary7677@gmail.com';
-const password = '123456789'
-
-let page;
-
-// open browser  it will return promise
-const browserOpen = puppeteer.launch({
-    headless: false,   // visibility
-    slowMo: true,
-    defaultViewport: null,
-    args: ["--start-maximized"] // fullscreen
-});
-
-browserOpen.then(function (browserContextObj){
-    const pagesArrPromise = browserContextObj.pages();
-    return pagesArrPromise;
-}).then(function(browserPages){
-    page = browserPages[0];
-    let hackerrankOpenPromise = page.goto(loginLink);
-    return hackerrankOpenPromise;
-}).then(function(){
-    let emailIsEntered = page.type("input[id='input-1']" , email ,{dalay: 50});
-    return emailIsEntered;
-}).then(function(){
-    let passwordIsEntered = page.type("input[id='input-2']" , password ,{dalay: 50});
-    return passwordIsEntered;
-}).then(function(){
-    let loginButtonPressed = page.click('button[data-analytics="LoginPassword"]' , {delay: 50});
-    return loginButtonPressed;
-}).then(function(){
-    let clickOnAlgoPromise = waitAndClick('.topic-card a[data-attr1="algorithms"]',page);
-    return clickOnAlgoPromise;
-}).then(function(){
-    let getToWarmUp = waitAndClick('.checkbox-input[value="warmup"]', page);
-    return getToWarmUp;
-}).then(function(){ // funs to select all questions , will give in array form
-    let allQuestions = page.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled" , {dalay: 5000}) // shorthand for document for queryselector all
-    return allQuestions;
-}).then(function(questionsArr){
-    console.log(`array length: ${questionsArr.length}`);
-    let questionWillBeSolved = questionSolver(page , questionsArr[0] , codeObj.answers[0]);
-    return questionWillBeSolved;
-}).catch(function(error){
-    console.log(error)
-})
+const password = '123456789';
 
 
-// wihtout opening the page , we cant target the elements 
-// will throw an error
+// using effi function to open the browser
 
-// this fns is bassically waiting for that particular element which takes some time to reload 
-// when we direct to the page , after then only it will choose that selecotr
+(async function (){
 
-function waitAndClick(selector , cPage){
-    return new Promise(function(resolve , reject){
-        let waitForModelPromise = cPage.waitForSelector(selector); // wait for the respective selector
-        waitForModelPromise.then(function(){
-            let clickModel = cPage.click(selector);
-            return clickModel;
-        }).then(function(){
-            resolve();
-        }).catch(function(){
-            reject();
-        })
-    })
+    try{
+        // open browser 
+        const browserOpen = await puppeteer.launch({
+        headless: false,   // to provide visibility to the browser
+        slowMo: true,
+        defaultViewport: null,
+        args: ["--start-maximized"] // fullscreen
+    });
+
+    // go to new tab of the opened browser
+    let newTab = await browserOpen.newPage();
+
+    // to the Hackerrank login page 
+    await newTab.goto(loginLink);
+
+    // enter email and password in respective field to login to account
+    await newTab.type("input[id='input-1']" , email ,{delay: 50});
+    await newTab.type("input[id='input-2']" , password ,{delay: 50});
+
+    // click on login button
+    await newTab.click('button[data-analytics="LoginPassword"]' , {delay: 50});
+
+    // go to algorithm section 
+    await waitAndClick('.topic-card a[data-attr1="algorithms"]',newTab);
+
+    // go to warmup section
+    await waitAndClick('.checkbox-input[value="warmup"]', newTab);
+
+    // get an array of all questions set available in warmup section
+    let allQuestions = await newTab.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled" , {delay: 5000}) // shorthand for document for queryselector all
+
+    // checking the length of array i.e total number of questions
+    console.log(`The length of array is ${allQuestions.length}`);
+
+    // calling the function to work on the problem by passing the fisrt question of the set and its answer
+    await  questionSolver(newTab , allQuestions[0] , codeObj.answers[0]);
+
+
+    }catch(error){
+        console.log(error);
+    }
+
+})()
+
+
+
+// defining waitAndClick function
+
+async function waitAndClick(selector , cPage){
+    await cPage.waitForSelector(selector); // wait for the respective selector
+
+    let selectorClicked = cPage.click(selector);
+    return selectorClicked;
 }
 
 
-function questionSolver(page , question , answer){
-    return new Promise(function(resolve , reject){
-        let questionWillBeClicked = question.click();
-        questionWillBeClicked.then(function(){ // here we want to go to the text area
-            let EditorInFocus = waitAndClick(".monaco-editor.no-user-select.vs" ,page);
-            return EditorInFocus;
-        })
-        .then(function(){
-            let customInputs = waitAndClick(".checkbox-input" , page);
-            return customInputs;
-        })
-        .then(function(){
-            return page.waitForSelector('textarea.custominput' , page);
-        })
-        .then(function(){
-            return page.type('textarea.custominput' , answer ,{delay: 10})
-        }).then(function(){
-            let ctrlIsPressed = page.keyboard.down('Control');
-            return ctrlIsPressed;
-        }).then(function(){
-            let AisPressed = page.keyboard.press('A' , {delay: 100});
-            return AisPressed;
-        }).then(function(){
-            let XisPressed = page.keyboard.press('X' , {delay: 100});
-            return XisPressed;
-        })
-        .then(function(){
-            let CtrlIsUnPressed = page.keyboard.up('Control');
-            return CtrlIsUnPressed;
-        }).then(function(){
-            let mainEditorInFocus = waitAndClick(".monaco-editor.no-user-select.vs" ,page);
-            return mainEditorInFocus;
-        }).then(function(){
-            let ctrlIsPressed = page.keyboard.down('Control');
-            return ctrlIsPressed;
-        }).then(function(){
-            let AisPressed = page.keyboard.press('A' , {dealy: 100});
-            return AisPressed;
-        }).then(function(){
-            let VisPressed = page.keyboard.press('V' , {dealy: 100});
-            return VisPressed;
-        }).then(function(){
-            let CtrlIsUnPressed = page.keyboard.up('Control');
-            return CtrlIsUnPressed;
-        }).then(function(){
-            return page.click('.hr-monaco__run-code' ,{delay: 50})
-        }).then(function(){
-            resolve();
-        }).catch(function(error){
-            reject();
-        })
-    })
+// defining the questionSolver function 
+
+async function questionSolver(page , question , answer){
+
+    // go the respective problem
+    await question.click({delay: 200});
+
+    // go to the editor section of the problem
+    await waitAndClick(".monaco-editor.no-user-select.vs" ,page , {delay: 100});
+    
+    // we cant directly code in the editor section as there we have autoclosing options for the braces of the function, so we will get to braces at a time,gonna throw error
+
+    // so to get to the test againts input section
+    await  waitAndClick(".checkbox-input" , page , {delay: 100});
+
+    // select the textarea where we will check our code
+    await page.waitForSelector('textarea.custominput' , page);
+
+    // type our code 
+    await page.type('textarea.custominput' , answer ,{delay: 100});
+   
+    // copy that written code
+    await page.keyboard.down('Control');
+
+    await page.keyboard.press('A' , {delay: 500});
+
+    await  page.keyboard.press('X' , {delay: 500});
+
+    await page.keyboard.up('Control');
+
+    // go to the editor section
+    await  waitAndClick(".monaco-editor.no-user-select.vs" ,page);
+
+    // paste the copied code
+    await  page.keyboard.down('Control');
+
+    await page.keyboard.press('A' , {delay: 500});
+
+    await page.keyboard.press('V' , {delay: 500});
+
+    await page.keyboard.up('Control');
+
+    // click run code button
+    await page.click('.hr-monaco__run-code' ,{delay: 500})
+
 }
+
+
